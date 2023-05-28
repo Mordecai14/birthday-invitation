ab24ca643076ea5b9f8610b296b85c7cec00e00b
Aramen Meza <arammendoza.97@gmail.com>
2023-05-27 23:13:02 -0600

    Agrega Intro para el sonido

--- src/components/TypingComputer/index.jsx
+++ src/components/TypingComputer/index.jsx
@@ -5,8 +5,12 @@ const TypingAnimation = ({ text, duration, loop }) => {
   const [typedText, setTypedText] = useState("");
   const [isDeleting, setIsDeleting] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isDisabled, setIsDisabled] = useState(false);
 
   useEffect(() => {
+    if (!isDisabled) {
+      return;
+    }
     const timeout = setTimeout(() => {
       let newText = typedText;
 
@@ -29,13 +33,34 @@ const TypingAnimation = ({ text, duration, loop }) => {
     }, duration);
 
     return () => clearTimeout(timeout);
-  }, [typedText, isDeleting, currentIndex, text, duration, loop]);
+  }, [typedText, isDeleting, currentIndex, text, duration, loop, isDisabled]);
 
   return (
-    <h1 className="text-green-500 text-5xl font-bold text-center w-screen h-[350px]">
-      {typedText}
-      <span className="animate-blink">|</span>
-    </h1>
+    <>
+      <section
+        className={`w-screen h-screen opacity-1 ${
+          isDisabled && "opacity-0"
+        } absolute z-10 transition-all`}
+        style={{
+          backgroundImage:
+            "url(https://i.pinimg.com/originals/11/83/7c/11837c0ee094b5e12f33fef3d41a1efa.jpg)",
+          backgroundSize: "cover",
+        }}
+        onClick={() => setIsDisabled(true)}
+      >
+        <h1 className="text-white">Hola, por favor activa tu audio :)</h1>
+        <button
+          className="text-white bg-purple-500 rounded-xl w-max px-4 py-2"
+          onClick={() => setIsDisabled(true)}
+        >
+          Continuar
+        </button>
+      </section>
+      <h1 className="text-green-500 text-5xl font-bold text-center w-screen h-[350px]">
+        {typedText}
+        <span className="animate-blink">|</span>
+      </h1>
+    </>
   );
 };
 
